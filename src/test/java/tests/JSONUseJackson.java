952635c5ad506140e4cff52577cd000e07f73876
delkabo <delkabo@yandex.ru>
2022-03-13 01:23:05 +0300

    added json reader jackson

---
+++ src/test/java/tests/JSONUseJackson.java
@@ -0,0 +1,31 @@
+package tests;
+
+
+import io.qameta.allure.internal.shadowed.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.zip.ZipFile;
+
+public class JSONUseJackson {
+
+    @Test
+    void parceJSON() {
+        try {
+            // create object mapper instance
+            ObjectMapper mapper = new ObjectMapper();
+            //ZipFile siZipov = new ZipFile("src/test/resources/zipfile.zip");
+            // convert JSON file to map
+            Map<?, ?> map = mapper.readValue(Paths.get("src/test/resources/book.json").toFile(), Map.class);
+
+            // print map entries
+            for (Map.Entry<?, ?> entry : map.entrySet()) {
+                System.out.println(entry.getKey() + "=" + entry.getValue());
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
